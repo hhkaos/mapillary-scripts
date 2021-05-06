8982008aca331ff86cc1143b2ed59030f40fc1f8
Raúl Jiménez Ortega <hhkaos@gmail.com>
2021-05-06 07:52:55 +0200

    First release

--- download.js
+++ download.js
@@ -1,21 +1,50 @@
-var parse = require('parse-link-header');
+const parse = require('parse-link-header');
+const config = require('./config.json');
+const axios = require('axios');
+const fs = require('fs')
 
-var linkHeader =
-  '<https://api.github.com/user/9287/repos?page=3&per_page=100>; rel="next", ' +
-  '<https://api.github.com/user/9287/repos?page=1&per_page=100>; rel="prev"; pet="cat", ' +
-  '<https://api.github.com/user/9287/repos?page=5&per_page=100>; rel="last"'
+let output = {"type":"FeatureCollection","features":[]}
+let page_counter = 1;
 
-var parsed = parse(linkHeader);
-console.log(parsed);
+const write_response = function(){
+    fs.writeFile(`downloads/images_downloaded_${Date.now()}.json`, JSON.stringify(output, null, 2), err => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        console.log(`Download finished: ${output.features.length} photos`);
+    });
+}
 
-var request = require('request');
-var options = {
-  'method': 'GET',
-  'url': 'https://a.mapillary.com/v3/images?client_id=&bbox=-2.4801851246529174,36.83570027690869,-2.424809739804093,36.86236923904206',
-  'headers': {
-  }
+const load_page = function(url){
+
+    axios.get(url)
+        .then(function (response) {
+            output.features = output.features.concat(response.data.features);
+            console.log(`Page ${page_counter} received`);
+            page_counter++;
+            if(response.headers.link || output.features === 400){
+                const parsed = parse(response.headers.link);
+                if(parsed.next){
+                    load_page(parsed.next.url);
+                }else{
+                    write_response();
+                }
+            }else{
+                console.log('No link header or more than 400 results')
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 };
-request(options, function (error, response) {
-  if (error) throw new Error(error);
-  console.log(response.body);
-});
+
+
+const u = new URLSearchParams({
+    client_id:config.client_id,
+    bbox: "-2.4801851246529174,36.83570027690869,-2.424809739804093,36.8623692390420",
+    // pano: true
+}).toString();
+
+load_page(`https://a.mapillary.com/v3/images?${u}`);
+
